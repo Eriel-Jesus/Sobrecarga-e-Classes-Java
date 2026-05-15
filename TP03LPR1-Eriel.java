17183069eb3cfe2bdaebb5ae0e44638b4feb94b0
Astravore <83691312+Eriel-Jesus@users.noreply.github.com>
2026-05-15 20:50:36 -0300

    Add files via upload
    Arquivo atualizado 1.0

---
+++ TP03LPR1-Eriel.java
@@ -0,0 +1,91 @@
+TP03Eriel
+
+import java.util.scanner;
+
+Scanner scanner = new Scanner(system.in);
+
+    public class Hora{
+
+         private int hora, min, seg;
+
+        public Hora(){
+            this.hora = 0;
+            this.min = 0;
+            this.seg = 0;
+        }
+
+        public Hora(int h, int m, int s){
+        this.hora = h;
+        this.min = m;
+        this.seg = s;
+        }
+
+        public void setHor(int h){
+            this.hora = h;
+        }
+        public void setMin(int m){
+            this.min = m;
+        }
+        setSeg(int s){
+            this.seg = s; 
+        }
+
+        setHor(){       
+            do
+            {
+            system.out.println("Insira o valor das horas:");
+            this.hora = scanner.next();
+            } while (this.hora > 24 || this.hora < 0);
+
+        }
+        setMin(){
+            do
+            {
+            system.out.println("Insira o valor dos minutos:");
+            this.min = scanner.next();
+            } while (this.min >= 60 || this.min < 0);
+        }
+        setSeg(){
+            do
+            {
+            system.out.println("Insira o valor dos segundos:");
+            this.seg = scanner.next();
+            } while (this.seg >= 60 || this.seg < 0);
+        }
+
+        int getHor(){
+            return this.hora;
+        }
+        int getMin(){
+            return this.min;
+        }
+        int getSeg(){   
+            return this.seg;
+        }
+
+        string getHora1(){
+            system.out.println(this.hora + ":" +this.min + ":" + this.seg);
+        }
+
+        string getHora2(){
+            int segundostotais, horapm;
+            if (this.hora > 12)
+                {   
+                    segundostotais = this.hora * 3600 + this.min * 60 + this.seg;
+                    horapm = this.hora - 12;
+                    system.out.println(horapm + ":" + this.min + ":" + this.seg + " PM");
+                }
+            else
+                {
+                segundostotais = this.hora * 3600 + this.min * 60 + this.seg;
+                system.out.println(this.hora + ":" + this.min + ":" + this.seg + " AM");
+                }
+
+        }
+
+        int getSegundos(){
+            int segundostotais;
+            segundostotais = this.hora * 3600 + this.min * 60 + this.seg;
+            return segundostotais;
+        }
+    }
